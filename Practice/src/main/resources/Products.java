86df5758246660eb06cfc8f01de1038faf8d8ff6
ASUS <ASUS@DESKTOP-LAQL1ON>
2025-06-27 14:45:09 +0530

    prod

--- Practice/src/main/resources/Products.java
+++ Practice/src/main/resources/Products.java
@@ -1,3 +1,11 @@
+package objectrepository.forcurrentTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class Products {
 
